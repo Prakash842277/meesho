174eef24696eb200422bfe3abbe42cb7484baeac
Prakash Chandra <prakash140chandra@gmail.com>
2023-04-13 13:58:27 +0530

    Create summary.js

---
+++ src/components/summary/summary.js
@@ -0,0 +1,24 @@
+import { useEffect } from "react";
+import BasicModal from "./modal";
+import { useStateContext } from "../context/Context";
+
+export default function Summary() {
+  const { setCartItems, setTotalPrice,setTotalQuantities } = useStateContext();
+
+    useEffect(() => {
+      window.scrollTo(0, 0);
+    }, []);
+    useEffect(()=>{
+      setCartItems([]);
+      setTotalPrice(0);
+      setTotalQuantities(0);
+    },[]);
+    return (
+      <div>
+        <BasicModal />
+        <h1 style={{ textAlign: "center", margin: "20px 0 250px" }}>
+          Order Placed
+        </h1>
+      </div>
+    );
+  }
